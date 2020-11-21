63a6ffed55b37cee407343e05f87f1c6665ee278
alinka770 <alytiger770@gmail.com>
2020-11-21 15:14:36 +0200

    home work complete

--- HW/src/HW.java
+++ HW/src/HW.java
@@ -1,8 +1,11 @@
+//Hlazkova Alina teudat 346316979
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HW {
-	
+	/* Напишите функцию по имени printLandingTime, которая получает время старта
+ракеты (часы, минуты,секунды), время полета в секундах и выводит на экран время
+приземления ракеты (часы, минуты, секунды) .*/
 	public static void printLandingTime(int h, int m, int s, int timeF)
 	{
 		int days = 0;
@@ -23,7 +26,6 @@ public class HW {
 			newH = newH - 24*days;
 		}
 		
-		if(newH > 1)
 		if (days == 0)
 		{
 		if(newH > 1)
@@ -109,11 +111,9 @@ public class HW {
 			else
 			{
 				if(newS > 1)
-					System.out.printf(" %d seconds%n",  newS);
+					System.out.printf("%d seconds%n",  newS);
 				else if(newS == 1)
 					System.out.printf("%d second%n", newS);
-				else
-					System.out.printf("Racket can not fly 0 seconds%n", newH);
 			}
 		}
 		}
@@ -202,11 +202,11 @@ public class HW {
 				else
 				{
 					if(newS > 1)
-						System.out.printf(" %d seconds  (+%d day) %n",  newS, days);
+						System.out.printf("%d seconds  (+%d day) %n",  newS, days);
 					else if(newS == 1)
 						System.out.printf("%d second  (+%d day) %n", newS, days);
 					else
-						System.out.printf("Racket can not fly 0 seconds%n", newH);
+						System.out.printf("%d day %n", days);
 				}
 			}
 		}
@@ -294,14 +294,16 @@ public class HW {
 				else
 				{
 					if(newS > 1)
-						System.out.printf(" %d seconds  (+%d days) %n",  newS, days);
+						System.out.printf("%d seconds  (+%d days) %n",  newS, days);
 					else if(newS == 1)
 						System.out.printf("%d second  (+%d days) %n", newS, days);
 					else
-						System.out.printf("Racket can not fly 0 seconds%n", newH);
+						System.out.printf("%d days%n", days);
 				}
 			}
 	}
+	
+	/*Проверка числа на четность*/
 	
 	public static boolean isEven(int number)
 	{
@@ -317,6 +319,7 @@ public class HW {
 		}
 		return true;
 	}
+	/*Проверка числа на нечетность*/
 	
 	public static boolean isOdd(int number)
 	{
@@ -332,7 +335,12 @@ public class HW {
 		}
 		return true;
 	}
-
+	
+/* Напишите функцию по имени getMax, которая получает от пользователя серию
+серию из 10 целых положительных чисел и возвращает максимальное число, которое
+состоит из четных и нечетных цифр.
+ */
+	
 	public static int getMax()
 	{
 		Scanner input = new Scanner(System.in);
@@ -351,6 +359,7 @@ public class HW {
 		}
 		return max;
 	}
+	/*Функция для проверки числа на симметричность*/
 	
 	public static boolean isSymmetrical(int number)
 	{
@@ -368,7 +377,11 @@ public class HW {
 			return false;
 		
 	}
-	
+	/*
+	 Напишите функцию по имени countSymmetricalNumbers, которая которая
+получает серию целых положительных чисел, значение 0 указывает на конец серии.
+Функция должна вернуть количество симметричных чисел
+	 */
 	public static int countSymmetricalNumbers()
 	{
 		int count = 0;
@@ -381,9 +394,14 @@ public class HW {
 				count++;	
 			x = input.nextInt();
 		}
+
 		return count;
 	}
-	
+	/*
+	 Напишите функцию по имени getGCD, которая получает два целых положительных
+числа и возвращает наибольший общий делитель этих двух целых чисел с помощью
+Алгоритма Евклида.
+*/
 	public static int getGCD(int x, int y)
 	{
 		while(x!=0 && y!=0)
@@ -397,10 +415,108 @@ public class HW {
 	}
 	
 	public static void main(String[] args) {
-//		printLandingTime(4,1,1, 176400);
-//		System.out.println(getMax());
-//		System.out.println(countSymmetricalNumbers());
-		System.out.println(getGCD(252, 105));
+		System.out.println("Working function printingLandTime\n");
+		System.out.println("----------------------------------------------\n");
+		printLandingTime(0,0,0, 1);  //1 second
+		printLandingTime(0,0,0, 30);  //30 seconds
+		printLandingTime(0,0,0, 60);  //1 minute
+		printLandingTime(0,0,0, 120); //2 minutes
+		printLandingTime(0,0,0, 3660);//1 hour, 1 minute
+		printLandingTime(0,0,0, 3661);//1 hour, 1 minute, 1 second
+		printLandingTime(0,0,0, 3690);//1 hour, 1 minute, 30 seconds
+		printLandingTime(0,0,0, 3720);//1 hour, 2 minutes
+		printLandingTime(0,0,0, 3730);//1 hour, 2 minutes, 10 seconds
+		printLandingTime(0,0,0, 86400);//1 day		
+		printLandingTime(0,0,0, 86401);//1 second (+1 day)
+		printLandingTime(0,0,0, 90000);//1 hour exactly(+1 day)
+		printLandingTime(0,0,0, 93600);//2 hours exactly(+1 day)
+		printLandingTime(0,0,0, 90061);//1 hour, 1 minute, 1 second (+ 1 day)
+		printLandingTime(0,0,0, 90125);//1 hour, 2 minutes, 5 seconds (+ 1 day)		
+		printLandingTime(0,0,0, 93725);//2 hours, 2 minutes, 5 seconds (+ 1 day)		
+		printLandingTime(0,0,0, 172800);//2 days
+		printLandingTime(0,0,0, 172801);//1 second (+2 days)
+		printLandingTime(0,0,0, 176400);//1 hour (+2 days)
+		printLandingTime(0,0,0, 180000);//2 hours (+ 2days)
+		printLandingTime(0,0,0, 180001);//2 hours, 1 second (+ 2 days)
+		printLandingTime(0,0,0, 180091);//2 hours, 1 minute, 31 seconds (+ 2 days)
+		printLandingTime(0,0,0, 180191);//2 hours, 3 minutes, 11 seconds (+ 2 days)
+		
+		char c = 'n';
+		int h, m, s, time;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Do you want to put yours time? If yes, press y, else press n");
+		c = input.next().charAt(0);
+		if(c == 'y')
+		{
+			System.out.println("Please input hours");
+			h = input.nextInt();
+			System.out.println("Please input minutes");
+			m = input.nextInt();
+			System.out.println("Please input seconds");
+			s = input.nextInt();
+			System.out.println("Please input time of flight");
+			time = input.nextInt();
+			printLandingTime(h, m, s, time);
+		}
+		System.out.println("\n----------------------------------------------\n");
+		System.out.println("Result of working function getMax()\n");
+		System.out.println("\nResult:    " + getMax());
+		System.out.println("\n----------------------------------------------\n");
+		System.out.println("Result of working function countSymmetricalNumbers()\n");
+		System.out.println("Result:   " + countSymmetricalNumbers());
+		System.out.println("\n----------------------------------------------\n");
+		System.out.println("Result of working function getGCD()\n");
+		System.out.println("\n----------------------------------------------\n");
+		System.out.println("Please, input two plus int numbers: ");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println("\nResult:   " + getGCD(a, b));
+		System.out.println("\n----------------------------------------------\n");
+		System.out.println("Creating examples of class ComplexNum and printing them\n");
+		System.out.println("\n**********************************************\n");
+		ComplexNum comp = new ComplexNum();
+		comp.printComplex();
+		
+		ComplexNum comp1 = new ComplexNum(3, 4);
+		comp1.printComplex();
+		ComplexNum comp2 = new ComplexNum(3, -4);
+		comp2.printComplex();
+		ComplexNum comp3 = new ComplexNum(-3, -4);
+		comp3.printComplex();
+		ComplexNum comp4 = new ComplexNum(-3, 4);
+		comp4.printComplex();
+		ComplexNum comp5 = new ComplexNum(0, -4);
+		comp5.printComplex();
+		ComplexNum comp6 = new ComplexNum(3, 0);
+		comp6.printComplex();
+		ComplexNum comp7 = new ComplexNum(comp2);
+		comp7.printComplex();
+		ComplexNum comp8 = new ComplexNum(4, 3);
+		comp8.printComplex();
+		System.out.println("Result of function get absolute for 3 + 4i:   "  + comp7.getAbsolute());
+		System.out.println("\n**********************************************\n");
+		System.out.println("Result of function addComplex for 3 + 4i and 3 - 4i: " );
+		comp1.addComplex(comp2).printComplex();
+		System.out.println("\n**********************************************\n");
+		System.out.println("Result of function addComplex for -3 + 4i and 3 + 4i: " );
+		comp1.addComplex(comp4).printComplex();
+		System.out.println("\n**********************************************\n");
+		System.out.println("Result of function addComplex for 3 + 4i and 4 + 3i: " );
+		comp1.addComplex(comp8).printComplex();
+		System.out.println("\n**********************************************\n");
+		System.out.println("Result of function equal for comp1 and comp2: " + comp1.equal(comp2));
+		System.out.println("\n**********************************************\n");
+		System.out.println("Result of function equal for comp7 and comp2: " + comp2.equal(comp7));
+		System.out.println("\n**********************************************\n");
+		System.out.println("Change the comp1 from 3 + 4i to 15 + 15i");
+		comp1.printComplex();
+		comp1.setImage(15);
+		comp1.setReal(15);
+		comp1.printComplex();
+		System.out.println("\n**********************************************\n");
+		System.out.println("Print the real of comp4 "  + comp4.getReal() +  " and image " + comp4.getImage());
+		System.out.println("\n**********************************************\n");
+
 	}
 
 }
